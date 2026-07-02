dc489cf0483e3219ac0c40fad7a70910d0afa822
Lizzy-kenya610 <elizabeth.muthengi@strathmore.edu>
2026-07-02 08:28:37 +0300

    Add form validation with user feedback

--- submission/js/script.js
+++ submission/js/script.js
@@ -15,41 +15,68 @@ let programs = [
     }
 ];
 programs.forEach(function (program) {
-   let card = document.createElement("div");
-   card.className = "program-card";
-   card.innerHTML = "<h3>" +program.name + "</h3>" +
-   "<p>" + program.description + "</p>";
+    let card = document.createElement("div");
+    card.className = "program-card";
+    card.innerHTML = "<h3>" + program.name + "</h3>" +
+        "<p>" + program.description + "</p>";
 
-   programList.appendChild(card);
+    programList.appendChild(card);
 });
 
 let wishlistInput = document.querySelector("#wishlistInput");
 let addWishlistButton = document.querySelector("#wishlistForm button");
 let wishlistItems = document.querySelector("#wishlistItems");
 
-addWishlistButton.addEventListener("click", event=> {
+addWishlistButton.addEventListener("click", event => {
     event.preventDefault();
-    if (wishlistInput.value!=""){
-    let li = document.createElement("li");
-    let button = document.createElement("button");
-    button.className = "delete-btn";
-    
-    li.textContent = wishlistInput.value;
-    button.textContent = "Delete";
-    button.addEventListener("click", event => {
-    li.remove();
-});
-    li.appendChild(button);
-    wishlistItems.appendChild(li);
-    wishlistInput.value = "";
+    if (wishlistInput.value != "") {
+        let li = document.createElement("li");
+        let button = document.createElement("button");
+        button.className = "delete-btn";
+
+        li.textContent = wishlistInput.value;
+        button.textContent = "Delete";
+        button.addEventListener("click", event => {
+            li.remove();
+        });
+        li.appendChild(button);
+        wishlistItems.appendChild(li);
+        wishlistInput.value = "";
 
     }
 
     else {
 
-    alert("Please enter a course.");
+        alert("Please enter a course.");
 
-}
+    }
 });
 
+let regForm = document.querySelector("#regform");
+let formMessage = document.querySelector("#form-message");
+//console.log(regForm);
+//console.log(formMessage);
+regForm.addEventListener("submit", event => {
+    event.preventDefault();
+    let parentName = document.querySelector("#parent-name");
+    let studentName = document.querySelector("#student-name");
+    let studentAge = document.querySelector("#student-age");
+    let phone = document.querySelector("#phone");
 
+    if (parentName.value == "" || studentName.value == "" || studentAge.value == "" || phone.value == "") {
+        formMessage.textContent = "Please fill in all the required fields";
+        formMessage.style.color = "red";
+    } else if (studentAge.value < 12 || studentAge.value > 15) {
+
+        formMessage.textContent = "Student age must be between 12 and 15 years.";
+        formMessage.style.color = "red";
+    } else {
+        formMessage.textContent = "Registration was succcessful for " + studentName.value + "!";
+        formMessage.style.color = "green";
+
+        parentName.value = "";
+        studentName.value = "";
+        studentAge.value = "";
+        phone.value = "";
+    }
+});
